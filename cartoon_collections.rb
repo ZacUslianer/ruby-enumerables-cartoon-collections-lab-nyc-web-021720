37faf36c805cef09f9a49b4d3a18939ffc17f676
Zac Uslianer <zacuslianer@gmail.com>
2020-02-08 22:05:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,11 @@ def summon_captain_planet(array)
   new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  new_array = []
+  i = 0 
+  while i < array.length do
+    
 end
 
 def find_the_cheese# code an argument here
