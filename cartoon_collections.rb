51f67bede9bb2384f6148e0c47e23d6c13aa822b
Zac Uslianer <zacuslianer@gmail.com>
2020-02-08 22:25:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ end
 i += 1
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(string)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
