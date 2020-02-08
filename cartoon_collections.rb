1fae9e16671f305814d850c996c753a9296c4d0e
Zac Uslianer <zacuslianer@gmail.com>
2020-02-08 20:19:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,6 +8,11 @@ end
 
 def summon_captain_planet(array)
   new_array = []
+  i = 0 
+  while i < array.length do 
+  new_array << array[i].capitalize + !
+  i += 1  
+  end
 end
 
 def long_planeteer_calls# code an argument here
